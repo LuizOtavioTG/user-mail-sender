bfeac0cffb9203164ff8d9acb09d8e4d301115bf
LuizOtavioTG <bc.luizo@gmail.com>
2025-07-23 22:11:38 -0300

    Feat: Criando fila de email.

--- email/src/main/java/dev/java10x/email/domain/EmailModel.java
+++ email/src/main/java/dev/java10x/email/domain/EmailModel.java
@@ -1,20 +1,27 @@
 package dev.java10x.email.domain;
 
 import dev.java10x.email.enums.EmailStatus;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Entity
 @Table(name = "TB_EMAIL")
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmailModel {
-    private String emailId;
-    private String userId;
+
+    @Transient
+    private final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID emailId;
+    private UUID userId;
     private String emailTo;
     private String emailFrom;
     private String emailSubject;
